d48834733ab160e041b011c1cb7bf0ae07254f65
MohammedDaniyalRaza <160598244+MohammedDaniyalRaza@users.noreply.github.com>
2025-07-06 14:24:49 +0500

    your message

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  images: {
+    domains: ['localhost', 'depizzatownstore.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
